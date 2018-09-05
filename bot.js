1a14f6e82938b30d7207b98364645cb570cc0251
fredriclam <fredriclam@live.com>
2018-09-05 04:15:21 -0400

    hello world

--- bot.js
+++ bot.js
@@ -3,7 +3,7 @@ var logger = require('winston');
 var auth = require('./auth.json');
 // Configure logger settings
 logger.remove(logger.transports.Console);
-logger.add(logger.transports.Console, {
+logger.add(new logger.transports.Console, {
     colorize: true
 });
 logger.level = 'debug';
@@ -20,20 +20,30 @@ bot.on('ready', function (evt) {
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Our bot needs to know if it will execute a command
     // It will listen for messages that will start with `!`
-    if (message.substring(0, 1) == '!') {
-        var args = message.substring(1).split(' ');
-        var cmd = args[0];
-       
-        args = args.splice(1);
-        switch(cmd) {
-            // !ping
-            case 'ping':
-                bot.sendMessage({
-                    to: channelID,
-                    message: 'Pong!'
-                });
-            break;
-            // Just add any case commands if you want to..
-         }
-     }
+
+    // if (message.substring(0, 1) == '!') {
+    console.log(message);
+
+    // Lower case command
+    var cmd = message.toLowerCase();
+
+    // Capture @mnhn329
+    if (cmd.includes('@mnhn329')){
+      // pass
+      bot.sendMessage({
+          to: channelID,
+          message: 'I see you pinged maxim :maximwhatsthis:'
+      });
+    }
+
+    // switch(cmd) {
+    //     // !ping
+    //     case '@mnhn329':
+    //         bot.sendMessage({
+    //             to: channelID,
+    //             message: 'Pong!'
+    //         });
+    //     break;
+    //     // Just add any case commands if you want to..
+    //  }
 });
