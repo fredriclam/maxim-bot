b512254ef9756c0f6295989113ed2c2258697e93
Fredriclam <fredriclam@live.com>
2018-10-29 21:54:40 -0700

    Refactor message handling, add file transport
    The message handling is refactored to clean up bot.on('message',
    ...). The quip is picked only when needed. Command actions (e.g.,
    !learn, !dump) are abstracted to appropriate functions.
    
    Created command action functions:
      * delayedMessage: sends a message to chat after fixed delay
      * logDump: dumps log to chat
      * learnTarget: switches to new target
      * pickQuip: reads message.log and picks a quip.
    
    A new logger transport that writes to file is created. The log
    is currently reused between executions of the bot, so it will
    dump logs from the previous run as well.
    
    To make the constants more readable, they are now attributes of
    bot and can be modified. Initialization of bot's extra
    attributes are grouped together, but these can be put in a new
    object.

--- app.js
+++ app.js
@@ -7,35 +7,54 @@ const weeabooChannelId = "215694187977375746"
 // Maxim data
 const MAX_ID = "163475101046538240";
 const FREDDY_ID = "265678340692770816";
-
-var TARGET_ID = MAX_ID;
 // Default quip
 let defaultMessage = 'Maxim afk :maximwhatsthis:';
 let maxsnuzyenEmoji = '<:maxsnuzyen:489283891807518720>';
 let coolstorybobEmoji = '<:coolstoryfred:503693902730100736>'
-// Configure logger settings
+
+// Message log file name
+messageLogName = 'messages.log'
+
+// Configure logger
+loggerFileName = './logger.log'
 logger.remove(logger.transports.Console);
 logger.add(new logger.transports.Console, {
     colorize: true
 });
+logger.add(new logger.transports.File({
+  filename: loggerFileName
+}))
 logger.level = 'debug';
+
 // Initialize Discord Bot
 var bot = new Discord.Client({
    token: auth.token,
-   autorun: true
+   autorun: true,
 });
-
-
-var botMessage = defaultMessage;
+// Append custom attributes
+// String proxy for invoking bot -- TODO
+bot.targetPlainString = () => '@mnhn329';
+// Message that bot will fire next
+bot.nextMessage = defaultMessage;
 // Flag to check for timer
 bot.isTimerOn = false;
 // Default learning target
-bot.learningTargetId = TARGET_ID;
+bot.learningTargetId = MAX_ID;
+// Default bank of quips is empty
+bot.quipList = [];
+// Timer delay (seconds)
+bot.secondsDelay = 5 * 1000;
+// Lines to output on dump
+bot.dumpLength = 5;
+// Max dump length
+bot.maxDumpLength = 20;
+// Next message bot will dump
+bot.nextMessage = defaultMessage;
+
 
 bot.on('ready', function (evt) {
-    logger.info('Connected');
-    logger.info('Logged in as: ');
-    logger.info(bot.username + ' - (' + bot.id + ')');
+    logger.info('=== Logged in as: ' + bot.username + ' - (' + bot.id + ')');
+    // Set presence shown on Discord
     bot.setPresence({
       game:{
       	name: "for mnhn329" // "Hide and Seek with "
@@ -43,99 +62,38 @@ bot.on('ready', function (evt) {
     })
     // Read chat history
     asyncParseToLog(weeabooChannelId, bot.learningTargetId);
-    
 });
 
 bot.on('message', function (user, userID, channelID, message, evt) {
     // Ignore self messages
     if (userID == bot.id){
-      logger.info('Own message detected! Ignoring.')
+      logger.debug('Ignoring own message.')
       return null;
     }
-
-    fs.readFile( __dirname + '/messages.log', function (err, data) {
-      if (err) throw err;      
-      // Pick quip for bot      
-      let quipList = data.toString().split('\n');
-			
-      quipListFiltered = quipList.filter(quip => quip.length > 0 && quip.charAt(0) != '!')
-      if (quipListFiltered && quipListFiltered.length != 0){
-      	botMessage = quipListFiltered[Math.floor(quipListFiltered.length * Math.random())] + ' ' + botEmoji(bot.learningTargetId);
-			} 
-			
-    });
-    // Log message to console
-    logger.info(message);
+    
+    // Log message intercepted from chat
+    logger.info("Message detected: " + message);
   
-
     let msg = message.toLowerCase();
-    // Capture ! commands
-    if (msg.substring(0, 1) == '!') {
+    
+    if (msg.substring(0, 1) == '!') { // Capture ! commands
       let args = msg.substring(1).split(' ');
       let cmd = args[0];
-      // logger.info(args);
+      logger.info("Inputs parsed: " + args)
       switch(cmd) {
         case 'learn':
-          // Extract largest number, whether or not enclosed by <@ ... >
-          let re = new RegExp(/\D*(\d+)/);
-          extractedId = re.exec(args[1]);
-          // Id validity check and logic
-          if (!(extractedId  === null)){
-            let newLearningTargetId = extractedId[1];
-            // Update server's user list
-            bot.getAllUsers((err) => { if (err) logger.warn(err); } );
-            // Accept new learning target if user exists
-            if (bot.users[newLearningTargetId] !== undefined){
-              bot.learningTargetId = newLearningTargetId
-              logger.info('New learning target: ' + bot.users[newLearningTargetId])
-              bot.setPresence({
-                game:{
-                	name: "for " + bot.users[newLearningTargetId].username
-                }
-              })
-
-							TARGET_ID = newLearningTargetId;
-              asyncParseToLog(channelID, newLearningTargetId);
-
-              bot.sendMessage({
-                to: channelID,
-                message: 'Now following <@' + newLearningTargetId + '>... <:maximwhatsthis:484993112729583618>'
-              });
-            }
-            else{
-              logger.warn('Invalid learning target selected')
-              bot.sendMessage({
-                to: channelID,
-                message: "<@" + userID + "> Wait, who's that? Tag 'em"
-              });
-            }
-          }
-          else {
-            bot.sendMessage({
-              to: channelID,
-              message: 'Technically, that ID is invalid <:maximwhatsthis:484993112729583618>'
-            });
-          }
+          learnTarget(bot, args[1], channelID);
+          break;
+        case 'dump':
+          console.log(bot.dumpLength)
+          logDump(bot, channelID);
           break;
       }
     }
-    else {
-      // Capture @mnhn329 messages
-      if (msg.includes(TARGET_ID) || msg.includes('@mnhn329')) {
-        if (!bot.isTimerOn){
-          // Start timer and payload if not on
-          bot.isTimerOn = true;
-          timer = setTimeout (function () {
-              // use the message's channel (TextChannel) to send a new message
-              bot.sendMessage({
-                  to: channelID,
-                  message: botMessage
-              });
-							logger.info(botMessage)
-              bot.isTimerOn = false;
-          }, 5 * 1000);
-          logger.info('Timer started');
-        }
+    else { // Capture @mnhn329 messages
+      if (msg.includes(bot.learningTargetId) || msg.includes(bot.targetPlainString)) {
+        pickQuip(bot);
+        delayedMessage(bot);
       }
       else if (msg.includes(bot.id)){
         bot.sendMessage({
@@ -144,21 +102,130 @@ bot.on('message', function (user, userID, channelID, message, evt) {
         });
       }
     }
-    // Timer cancel
-    if (msg.includes('!maximback') || String(userID) == TARGET_ID){
+
+    // Special conditions for canceling timer
+    if (msg.includes('!maximback') || String(userID) == bot.learningTargetId){
       try{
         clearTimeout(timer);
-        logger.info('Timer stopped');
+        logger.info('Timer stopped.');
       }
       catch(error){
-        logger.info("hehe probably timer didn't exist");
+        logger.info("Probably timer didn't exist; following timer stop error was caught: ");
+        logger.info(error)
       }
       bot.isTimerOn = false;
     }
 });
 
-// Asynchronously parse target user's chat history into log
+/**
+ * Sends bot.nextMessage after delay given by bot.secondsDelay to channelID
+ * @param {Discord.Client} bot 
+ * @param {str} channelID
+ */
+function delayedMessage(bot){
+  if (!bot.isTimerOn){
+    bot.isTimerOn = true;
+    // Set new timer
+    bot.timer = setTimeout(function () {
+        bot.isTimerOn = false;
+        bot.sendMessage({
+            to: channelID,
+            message: bot.nextMessage
+        });        
+    }, bot.secondsDelay);
+    logger.info('Timer started');
+  }
+}
 
+
+/**
+ * Dumps log to Discord chat.
+ * Uses logger, and fires message to channelID.
+ * Uses fs to load logger file asynchronously (Winston logger broken)
+ * Uses global __dirname, messageLogName.
+ * @param {Discord.Client} bot 
+ * @param {str} channelID
+ */
+function logDump(bot, channelID){
+  // Read log and dump
+  fs.readFile(`${__dirname}/${loggerFileName}`, function(err, data){
+    if (err) logger.error("Error caught reading logger file: " + err.message);
+    // Sanitize input
+    logString = data.toString().replace("```","").split('\n').slice(-bot.dumpLength-1,-1).join('\n');
+    logString = "Debug log ():\n```[...]\n" + logString + "```";
+    // DUmp to chat
+    bot.sendMessage({
+      to: channelID,
+      message: logString
+    }, function(err) {if (err) logger.error("Error dumping logs: " + err.message)})
+  })
+}
+
+/** 
+ * Switches bot to a new target.
+ * Sets bot.learningTargetId and presence of bot using bot.setPresence.
+ * Uses logger and fires messages to channelID
+ * @param {Discord.Client} bot
+ * @param {str} target: raw string (second argument of command sent to bot).
+ * @param {str} channelID
+ */ 
+function learnTarget(bot, target, channelID) {
+  // Extract largest number, whether or not enclosed by <@ ... >
+  let re = new RegExp(/\D*(\d+)/);
+  extractedId = re.exec(target);
+  logger.info(extractedId + target)
+  let msg = {to: channelID}
+  if (!(extractedId === null)){
+    let newLearningTargetId = extractedId[1];
+    // Update server's user list
+    bot.getAllUsers((err) => { if (err) logger.error("Error caught in learnTarget: " + err.message); } );
+    // Accept new learning target if user exists
+    if (bot.users[newLearningTargetId] !== undefined){
+      bot.learningTargetId = newLearningTargetId
+      bot.setPresence({
+        game:{
+          name: "for " + bot.users[bot.learningTargetId].username
+        }
+      })
+      // Parse log
+      asyncParseToLog(channelID, newLearningTargetId);
+      logger.info('New learning target: ' + bot.users[newLearningTargetId].username)
+      msg.message = 'Now following <@' + newLearningTargetId + '>... <:maximwhatsthis:484993112729583618>'
+    }
+    else{
+      logger.warn('Invalid learning target selected')
+      msg.message = "<@" + userID + "> Wait, who's that? Tag 'em"
+    }
+  }
+  else {
+    logger.warn('Unexpected learning target')
+    msg.message = 'Technically, that ID is invalid <:maximwhatsthis:484993112729583618>'
+  }
+  // Fire message
+  bot.sendMessage(msg)
+}
+
+/** 
+ * Pick a quip and load it into bot.nextMessage
+ * Uses fs to load file message log file asynchronously.
+ * Uses global __dirname, messageLogName.
+ * @param {Discord.Client} bot
+ */
+function pickQuip(bot){
+  fs.readFile( `${__dirname}/${messageLogName}`, function (err, data) {
+    if (err) logger.error("Error caught reading message log: " + err.message);
+    // Read all quips from data
+    bot.quipList = data.toString().split('\n');
+    // Filter out useless elements and !commands
+    quipListFiltered = quipList.filter(quip => quip.length > 0 && quip.charAt(0) != '!')
+    // Load bot's next message
+    if (quipListFiltered && quipListFiltered.length != 0){
+      bot.nextMessage = quipListFiltered[Math.floor(quipListFiltered.length * Math.random())] + ' ' + botEmoji(bot.learningTargetId);
+    } 
+  });
+}
+
+// Asynchronously parse target user's chat history into log
 function asyncParseToLog(channelID, targetUserID){
   // Read chat history
 let allBatches = [];
@@ -175,7 +242,7 @@ let allMessages = getMessagesCallback(allBatches, beginningOfMessages, prevMessa
       }
 		
 
-      fs.writeFile("messages.log", formattedMessageLog, (err) => {
+      fs.writeFile(`${messageLogName}`, formattedMessageLog, (err) => {
         if (err) throw err;
         logger.info('The file has been saved!');
       }); 
